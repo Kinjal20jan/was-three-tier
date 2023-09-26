2171a3563ad4ec67472c3c8be4625a67b8edbb1e
Kinjal20jan <thakkarkinjal2000@gmail.com>
2023-09-26 12:52:49 +0530

    DBConfig changes

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1-instance-1.crwgz9x1r61y.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'kinjal2000',
+    DB_DATABASE : 'webappdb'
 });
